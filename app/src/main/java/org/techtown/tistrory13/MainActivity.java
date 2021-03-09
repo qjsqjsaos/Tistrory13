638f85084054ecb15a34bcc93d95dec765749b48
Beaksooyeol <merrygoaround0726@gmail.com>
2021-03-09 22:30:43 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/tistrory13/MainActivity.java
@@ -0,0 +1,59 @@
+package org.techtown.tistrory13;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+
+    //프로필 사진 요청코드
+    private static final int REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() { //갤러리에 요청코드 보내기
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Uri uri = data.getData();
+                    Glide.with(getApplicationContext()).load(uri).into(imageView); //다이얼로그 이미지사진에 넣기
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {// 취소시 호출할 행동 쓰기
+            }
+        }
+    }
+
+}
